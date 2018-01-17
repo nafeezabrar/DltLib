d7e34bce2cf48e161b9a7ae2c8610ab1163c9743
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-17 16:04:48 +0600

    DltPacket class added

---
+++ DltLib/Data/DltPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace DltLib.Data
+{
+    public class DltPacket
+    {
+        public long Address { get; set; }
+        public byte ControlByte { get; set; }
+        public byte[] DataBlock { get; set; }
+    }
+}
