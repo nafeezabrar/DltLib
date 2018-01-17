4b2764290ec1a6b6922ca38be8ad7a6552d97894
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-17 19:37:38 +0600

    DLT Constants are added

---
+++ DltLib/Constants/DltConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DltLib.Constants
+{
+    public static class DltConstants
+    {
+        public const byte FrameStartByte = 0x68;
+        public const byte FrameStopByte = 0x16;
+    }
+}
