941edaa2ff1825d30784af70d8e2d7e8d1ee9815
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-17 19:43:50 +0600

    Custom assert implemented

---
+++ DltLib.Tests/Util/DltLibAssert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DltLib.Tests.Util
+{
+    public static class DltLibAssert
+    {
+        public static void AssertException<TException>(Action action)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail($"Expected {typeof(TException).FullName} is not thrown");
+            }
+            catch (TException)
+            {
+            }
+        }
+
+        public static void AssertException<TException>(Action action, Predicate<TException> isExpectedException)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail($"Expected {typeof(TException).FullName} is not thrown");
+            }
+            catch (TException ex)
+            {
+                if (!isExpectedException(ex))
+                {
+                    Assert.Fail($"Exception {ex.GetType().FullName} could not meet expectation");
+                }
+            }
+        }
+    }
+}
