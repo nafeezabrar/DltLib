03c27d3c3031d2c597feead6a76d01584dfe1486
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-17 16:09:05 +0600

    DltLibException class is added

---
+++ DltLib/Exceptions/DltLibException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DltLib.Exceptions
+{
+    public class DltLibException : Exception
+    {
+        public DltLibException()
+        {
+        }
+
+        public DltLibException(string message) : base(message)
+        {
+        }
+
+        public DltLibException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
