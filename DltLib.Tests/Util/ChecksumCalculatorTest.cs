5218a1f858fde0c95c8c5b34ab48efaa49b435fe
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-17 18:21:21 +0600

    ChecksumCalculator implemented

---
+++ DltLib.Tests/Util/ChecksumCalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using DltLib.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DltLib.Tests.Util
+{
+    [TestClass]
+    public class ChecksumCalculatorTest
+    {
+        private ChecksumCalculator _checksumCalculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _checksumCalculator = new ChecksumCalculator();
+        }
+
+        [TestMethod]
+        public void CalculateChecksumCorrectly()
+        {
+            Assert.AreEqual(0xC3, _checksumCalculator.CalculateChecksum(new byte[]
+            {
+                0x68, 0x27, 0x19, 0x40, 0x23, 0x74, 0x68, 0x68, 0x11, 0x04, 0x34, 0x35, 0xC3, 0x33, 0xC3, 0x16
+            }, 0, 13));
+            Assert.AreEqual(0x7C, _checksumCalculator.CalculateChecksum(new byte[]
+            {
+                0x68, 0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x68, 0x91, 0x08, 0x34, 0x35, 0xC3, 0x33, 0x5C, 0x3C, 0x33,
+                0x33, 0x7C, 0x16
+            }, 0, 17));
+        }
+    }
+}
---
+++ DltLib/Util/ChecksumCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace DltLib.Util
+{
+    public class ChecksumCalculator
+    {
+        public byte CalculateChecksum(byte[] bytes, int start, int end)
+        {
+            byte checksum = 0;
+            for (var i = start; i <= end; i++)
+                checksum += bytes[i];
+
+            return checksum;
+        }
+    }
+}
