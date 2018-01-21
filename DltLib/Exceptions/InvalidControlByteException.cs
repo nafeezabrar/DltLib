dc8095b1f87ea41767874f605f40bc75bf975857
Nafeez Abrar <nafeezabrar@gmail.com>
2018-01-21 15:49:59 +0600

    InvalidControlByteException added

---
+++ DltLib/Exceptions/InvalidControlByteException.cs
@@ -0,0 +1,13 @@
+﻿namespace DltLib.Exceptions
+{
+    public class InvalidControlByteException : InvalidDltDataException
+    {
+        public byte InvalidControlByte { get; }
+
+        public InvalidControlByteException(byte invalidControlByte)
+            : base($"Invalid control byte 0x{invalidControlByte:X2}")
+        {
+            InvalidControlByte = invalidControlByte;
+        }
+    }
+}
